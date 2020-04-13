a5b7f32a72cf2cffa1dcfbce60fc21554b537750
Nancyyao <amy123881@gmail.com>
2020-04-14 00:22:00 +0800

    複習到0406-am22:45

---
+++ 0406/Forms.php
@@ -0,0 +1,43 @@
+<?php $title = 'Index'; ?>
+<!--設定頁面名稱(總網站名稱設定在_head.php)-->
+
+<?php include __DIR__ . '/parts/_head.php' ?>
+<?php include __DIR__ . '/parts/_navbar.php' ?>
+<!--
+  include:要不到會提示woring
+  require:要不到直接停止運作error
+-->
+
+<div class="container">
+  <pre>
+  <?php
+  print_r($_GET);
+  //得到一個array(關聯式陣列)
+  //要送出一定要有NAME屬性!!!!!!!!
+  ?>
+  </pre>
+
+  <form method="get" action="">
+    <!-- method="傳送方式" action="傳給誰，沒設定就傳給自己"-->
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="text" class="form-control" id="email" name="email">
+      <small id="emailHelp" class="form-text text-muted"></small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="Password1" name="Password1">
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="Check1" name="Check1">
+      <label class="form-check-label" for="Check1">Check me out</label>
+      <!-- 相關選擇方式請看mmmh57-php-0406-am(https://youtu.be/N0HmwxJJ4b4?t=1536) -->
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
+</div>
+
+
+<?php include __DIR__ . '/parts/_script.php' ?>
+<?php include __DIR__ . '/parts/_foot.php' ?>
---
+++ 0406/parts/_head.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title><?= isset($title) ? $title.' - ' :'' ?>NancyHomeStudy</title>
+    <!-- 如果設定了title就變成title的字串+NancyHomeStudy -->
+    <!--設定頁面名稱(總網站名稱設定在_head.php)-->
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
