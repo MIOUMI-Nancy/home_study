704dfd460e138f386460da04c126fa25f5bc9020
NancyYao <amy123881@gmail.com>
2020-04-16 15:23:25 +0800

    0416_還有死神老師的加課

--- 0415/SCHOOL_mmmh57-php-master/add-to-cart-api.php
+++ 0415/SCHOOL_mmmh57-php-master/add-to-cart-api.php
@@ -1,27 +1,27 @@
 <?php
-
 /*
  * 新增, 修改, 移除
  */
 
 session_start();
 
-if (!isset($_SESSION['car'])) {
-    $_SESSION['car'] = [];
+if(! isset($_SESSION['cart'])){
+    $_SESSION['cart'] = [];
 }
 
-$sid = isset($_GET['sid']) ? intval($_GET['sid']) : '';
-$qty = isset($_GET['qty']) ? intval($_GET['qty']) : '';
+$sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
+$qty = isset($_GET['qty']) ? intval($_GET['qty']) : 0;
 
-if (!empty('$sid')) {
-    // TODO: 判斷車裡有沒有那個商品
-    if (!empty('$qty')) {
-        //有$SID又有提供$QTY(數量)>>>>修改或新增
+if(! empty($sid)){
+    // TODO: 判斷有沒有那個商品
+
+    if(! empty($qty)){
+        // 新增或修改
         $_SESSION['cart'][$sid] = $qty;
-    }else{
+    } else {
+        // 移除
         unset($_SESSION['cart'][$sid]);
     }
-    
 }
 header('Content-Type: application/json');
 echo json_encode($_SESSION['cart']);
